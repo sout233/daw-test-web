8db18bec786a1cf36e7415333c98c8e337c58a5a
sout_Nantang <55623894+sout233@users.noreply.github.com>
2024-06-27 12:18:25 +0800

    Update ResultPage.jsx

--- src/ResultPage.jsx
+++ src/ResultPage.jsx
@@ -25,6 +25,7 @@ function ResultPage() {
         'Studio One': '/Studio One.png',
         'Cakewalk': '/Cakewalk.png',
         'LMMS': '/LMMS.svg',
+        'Bitwig': 'https://assets.bitwig.net/static/web/img/BW_Logo_Header.svg?v=001'
     }
 
 
@@ -39,8 +40,8 @@ function ResultPage() {
                 fillOpacity: 0.2,
             },
         },
-        height:400,
-        width:400,
+        height: 400,
+        width: 400,
         scale: {
             x: {
                 padding: 0.5,
@@ -119,27 +120,28 @@ function ResultPage() {
     }, []);
 
     return (
-        <div className="max-w-4xl overscroll-x-none flex flex-col items-center justify-center m-auto">
+        <div className="max-w-4xl w-screen overscroll-x-none flex flex-col items-center justify-center m-auto">
             <div className="flex flex-col justify-center">
+                <div className="overflow-hidden w-screen h-screen absolute translate-y-[30%]">
+                    <div className="bg-primary pointer-events-none absolute start-20 aspect-square w-96 rounded-full opacity-20 blur-3xl [transform:translate3d(0,0,0)]"></div>
+                    <div className="bg-success pointer-events-none absolute aspect-square w-full rounded-full opacity-10 blur-3xl [transform:translate3d(0,0,0)]"></div>
+                    <img src={logos[aimDaw]} alt="logo" className="w-60 absolute rotate-6 rounded-2xl translate-x-[120%] translate-y-[50%] opacity-50" />
+                </div>
                 <div className="m-10">
                     <div className="flex flex-row justify-between">
-                        <h1 className="text-2xl font-bold text-base-content">测测哪款DAW更适合你</h1>
-                        <h1 className="text-2xl font-bold text-primary italic">测试结果</h1>
+                        <h1 className="md:text-2xl text-xl font-bold text-base-content">测测哪款DAW更适合你</h1>
+                        <h1 className="md:text-2xl text-xl font-bold text-primary italic">测试结果</h1>
                     </div>
                     <div class="divider"></div>
-                    <div className="flex flex-row overscroll-x-none justify-between overflow-hidden">
+                    <div className="flex flex-row justify-between">
                         <div className="flex flex-col">
-                            <h1 className="text-4xl font-bold text-base-content">或许</h1>
-                            <h1 className="text-4xl font-bold text-base-content"><span className="text-primary">{aimDaw}</span> 比较适合你？</h1>
+                            <h1 className="md:text-4xl text-3xl font-bold text-base-content">或许</h1>
+                            <h1 className="md:text-4xl text-3xl font-bold text-base-content"><span className="text-primary">{aimDaw}</span> 比较适合你？</h1>
                             <h1 className="text-xl font-bold text-base-content">其次是{sortedEntries[1][0]}和{sortedEntries[2][0]}</h1>
                             <h1 className="text-xl font-bold text-base-content">它们可能也是不错的选择</h1>
                         </div>
-                        <img src={logos[aimDaw]} alt="logo" className="w-60 absolute rotate-6 rounded-2xl translate-x-[100%] translate-y-[-10%] top- opacity-50" />
                     </div>
-
                 </div>
-                <div className="bg-primary pointer-events-none absolute start-20 aspect-square w-96 rounded-full opacity-20 blur-3xl [transform:translate3d(0,0,0)]"></div>
-                <div className="bg-success pointer-events-none absolute aspect-square w-full rounded-full opacity-10 blur-3xl [transform:translate3d(0,0,0)]"></div>
             </div>
             <div className="flex flex-col justify-center items-center bg-white rounded-xl p-4 m-4">
                 {listItems}
@@ -150,11 +152,24 @@ function ResultPage() {
             <div className="flex flex-col justify-between items-center">
                 <h1 className="text-4xl font-bold text-base-content">{aimDaw}</h1>
                 <h1 className="text-6xl font-bold text-primary italic absolute left-[50%] translate-x-[-50%] translate-y-[-50%] opacity-20 text-nowrap">{recommend && recommend[aimDaw]['tag']}</h1>
-                <p className="pt-5 text-lg font-bold text-base-content text-pretty break-before-auto whitespace-pre-line text-center">{recommend && recommend[aimDaw]['desc']}</p>
+                <p className="pt-5 text-lg p-10 font-bold text-base-content text-pretty break-before-auto whitespace-pre-line text-center">{recommend && recommend[aimDaw]['desc']}</p>
             </div>
 
             <div className="h-20"></div>
-            <Radar {...config} ></Radar>
+
+            <div className="items-center justify-center text-center">
+                <h1 className="text-4xl font-bold text-base-content mb-10">?看看参数?</h1>
+                <Radar {...config} ></Radar>
+            </div>
+
+            <div className="h-20"></div>
+
+            <div className="flex flex-col justify-center items-center">
+                <button className="btn btn-primary w-60">生成报告</button>
+            </div>
+
+            <div className="h-20"></div>
+
         </div>
     );
 }
